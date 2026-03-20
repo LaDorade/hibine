ced4a9e1a698a1e4f1ccfe8348b23274634efcd6
Mathis <mathisjung02@gmail.com>
2026-03-20 13:20:19 +0100

    dont hide searchbar on mobile

--- src/routes/(app)/tape/[tape]/+page.svelte
+++ src/routes/(app)/tape/[tape]/+page.svelte
@@ -2,7 +2,6 @@
   import SearchBar from '$components/SearchBar.svelte';
   import Pane from './Pane.svelte';
   import BottomNav from './BottomNav.svelte';
-  import { viewportStore } from '$stores/Viewport.svelte.js';
   import { getCurrentTape } from '$lib/remotes/files.remote';
   import { stopEvent } from '$lib/utils';
   import { coreAPI } from '$core/CoreAPI.svelte';
@@ -31,9 +30,8 @@
   await coreAPI.pageStore.openFromUrl();
 }} />
 
-{#if !viewportStore.isMobile}
-  <SearchBar bind:searchBarOpen />
-{/if}
+<!-- Todo: Make as option ? -->
+<SearchBar bind:searchBarOpen />
 
 <svelte:head>
   <title>{await getCurrentTape()} – Hibine</title>
